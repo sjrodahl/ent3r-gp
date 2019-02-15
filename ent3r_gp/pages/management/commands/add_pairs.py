198ce18bcdf49d76f5b8aef7238f7b556e75d0bb
Sondre Rodahl <son.rodahl@gmail.com>
2019-02-15 14:30:40 +0100

    Highscorelist for mentor pair
    Instead of individually

--- ent3r_gp/pages/management/commands/add_pairs.py
+++ ent3r_gp/pages/management/commands/add_pairs.py
@@ -18,14 +18,14 @@ class Command(BaseCommand):
                 groupname, mentor_1, mentor_2, room, campus, comment = row
                 mentor_1_last_name = mentor_1.split()[-1]
                 mentor_2_last_name = mentor_2.split()[-1]
-                user_1 = User.objects.get(last_name=mentor_1_last_name)
-                user_2 = User.objects.get(last_name=mentor_2_last_name)
-                pair = MentorPair.objects.create(name=groupname, mentor_1=user_1, mentor_2=user_2)
+                try:
+                    user_1 = User.objects.get(last_name=mentor_1_last_name)
+                    user_2 = User.objects.get(last_name=mentor_2_last_name)
+                except User.DoesNotExist:
+                    print(mentor_1_last_name )
+                    print(mentor_2_last_name )
 
-
-
-
-
-
-
+                MentorPair.objects.create(name=groupname, mentor_1=user_1, mentor_2=user_2)
+                # Create the mirrored pair as well
+                MentorPair.objects.create(name=groupname, mentor_1=user_2, mentor_2=user_1)
 
