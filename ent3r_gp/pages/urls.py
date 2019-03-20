ffa21a12ef853ef60e9eec579f31ec5e6aa7fe18
Sondre Rodahl <son.rodahl@gmail.com>
2019-03-20 11:49:16 +0100

    Updated urls to django2 format

--- ent3r_gp/pages/urls.py
+++ ent3r_gp/pages/urls.py
@@ -1,16 +1,20 @@
-from django.conf.urls import url
+from django.urls import path, register_converter
 from django.contrib.auth import views as auth_views
-from pages import views
+
+from pages import views, converters
+
+register_converter(converters.FourDigitYearConverter, 'yyyy')
+register_converter(converters.OneOrTwoDigitMonthConverter, 'mm')
 
 urlpatterns = [
 
-        url(r'activities/', views.activities, name ='pages_activities'),
-        url(r'hiscore/$', views.hiscore, name = 'pages_hiscore'),
-        url(r'hiscore/(?P<year>[0-9]{4})/(?P<period>[0-9]{1,2})/$', views.hiscore),
-        url(r'hiscore/total/$', views.hiscore_total, name='pages_hiscore_total'),
-        url(r'new_activity/', views.activity_new, name = 'pages_new_activity'),
-        url(r'my_achievements', views.my_achievements, name = 'pages_my_achievements'),
-        url(r'del_achievements', views.delete_achievements, name = 'pages_del_achievements'),
-        url(r'del_activities', views.delete_activities, name = 'pages_del_activities'),
-        url(r'^$', views.index, name = 'pages_index'),
+        path('activities/', views.activities, name='pages_activities'),
+        path('hiscore/', views.hiscore, name='pages_hiscore'),
+        path('hiscore/<yyyy:year>/<mm:period>/', views.hiscore),
+        path('hiscore/total/', views.hiscore_total, name='pages_hiscore_total'),
+        path('new_activity/', views.activity_new, name='pages_new_activity'),
+        path('my_achievements/', views.my_achievements, name='pages_my_achievements'),
+        path('del_achievements/', views.delete_achievements, name='pages_del_achievements'),
+        path('del_activities/', views.delete_activities, name='pages_del_activities'),
+        path('', views.index, name='pages_index'),
         ]
